f879076ad625562703c3c623b8b3dac43c47b095
Olena-Synytsia <alenasinitsa85@gmail.com>
2025-02-10 18:37:08 +0200

    add LocationFilter html+css

--- src/components/FiltersWrap/LocationFilter/LocationFilter.jsx
+++ src/components/FiltersWrap/LocationFilter/LocationFilter.jsx
@@ -1,10 +1,21 @@
+import { IoMapOutline } from "react-icons/io5";
 import s from "./LocationFilter.module.css";
 
 const LocationFilter = () => {
   return (
     <div className={s.container}>
-      <div className={s.text}>Location</div>
-      <div>Location filter input</div>
+      <label className={s.label}>Location</label>
+      <div className={s.inputWrap}>
+        <IoMapOutline className={s.icon} />
+
+        <input
+          type="text"
+          className={s.input}
+          // onChange={handleChange}
+          placeholder="Enter location"
+          // value={location}
+        />
+      </div>
     </div>
   );
 };
